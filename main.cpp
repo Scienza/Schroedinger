f3e4528da892b39c09da0f68e89a3e45c3a6a466
Paolo <dedonato95@hotmail.it>
2018-01-10 18:28:58 +0100

    Moved test functions in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <Schroedinger>
 
+double potential(double);
+void solve_Numerov(double, double, double, int, double (*potential)(double), double *);
+
+int main() {
+    int nbox = 1000;
+    double step = 0.01, norm;
+    double *wavefunction = new double[nbox];
+    //wavefunction initialization
+    wavefunction[0] = 0.;
+    wavefunction[1] = 0.2; //later on it gets renormalized, so is just a conventional number
+    solve_Numerov(0., 2., step, nbox, &potential, wavefunction);
+
+    return 0;
+}
+
+/* potential function defines the potential in function of position */
+double potential(double x) {
+    double value = x * x;
+    return value;
+} // end of function to evaluate
