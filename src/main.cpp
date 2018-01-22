3c434dfb17df0c1f66da2975a2270e5052bf8a81
Andrea Idini <andrea.idini@gmail.com>
2018-01-22 22:31:07 +0000

    test on harmonic oscillator function

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,25 @@ TEST(TrivialTest, one) {
   EXPECT_EQ(1, 1);
 }
 
+// TEST(ParameterTest, one) {
+//   EXPECT_TRUE(nbox > 100);
+// }
+
+TEST(WfTest,HarmonicOscillator){
+  int nbox = 1000;
+  double *numerov_Wf = new double[nbox];
+  double *analytic_Wf = new double[nbox];
+
+  numerov_Wf[0] = 0.;
+  numerov_Wf[1] = 0.2; //later on it gets renormalized, so is just a conventional number
+  solve_Numerov(0., 2., 0.01, nbox, &potential, numerov_Wf);
+
+  analytic_Wf = harmonic_wf(1,nbox, 1.);
+  for(int i; i < nbox; i++){
+    ASSERT_FLOAT_EQ(numerov_Wf[i],analytic_Wf[i]);
+  }
+}
+
 }  // namespace
 
 /* potential function defines the potential in function of position */
