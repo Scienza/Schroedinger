af1dc61204e4ff5905aee68f68e2346f0d3edc6b
Andrea Idini <andrea.idini@gmail.com>
2018-01-22 00:10:08 +0000

    implementing googletest (works?)

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,16 @@
 #include "test.h"
 #include "gtest/gtest.h"
 
+#include <string>
+
 double potential(double);
 
-int main() {
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
     int nbox = 1000;
     double step = 0.01, norm;
     double *wavefunction = new double[nbox];
     std::string mode = "TEST";
-
 
     if (mode == "TEST") {
       // wavefunction = box_wf(2,nbox);
@@ -24,8 +26,16 @@ int main() {
     }
     //wavefunction initialization
 
-    return 0;
+    return RUN_ALL_TESTS();
 }
+
+
+using std::string;
+
+const char *actualValTrue  = "hello gtest";
+const char *actualValFalse = "hello world";
+const char *expectVal      = "hello gtest";
+
 
 /* potential function defines the potential in function of position */
 double potential(double x) {
