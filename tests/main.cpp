60ef4296d0720c96a8e25087dd68014b629eb91c
Andrea Idini <andrea.idini@gmail.com>
2018-02-14 17:53:32 +0100

    implemented different properties test, careful BoxTest Failing

--- tests/main.cpp
+++ tests/main.cpp
@@ -77,6 +77,39 @@ namespace {
         ASSERT_NEAR(E_numerov, E_analytic, err*10 );
 
     }
+
+    TEST(WfTest,Box2){
+        unsigned int nbox = 1000;
+        double *numerov_Wf = new double[nbox+1];
+        double *analytic_Wf = new double[nbox+1];
+        std::vector<double> x(nbox), pot;
+
+        for(std::vector<int>::size_type i = 0; i < x.size(); i++){
+            x[i] = dx * (int) (i - nbox / 2);
+        }
+
+//        Potential V(x, "box", 0.5);
+        Potential::Builder b(x);
+        Potential V = b.setType("box")
+                .build();
+
+        numerov_Wf[0] = 0.;
+        numerov_Wf[1] = 0.2; //later on it gets renormalized, so is just a conventional number
+
+        double E_numerov = solve_Numerov(0., 2., 0.01, nbox, V, numerov_Wf);
+
+        double E_analytic = box_wf(1,nbox, analytic_Wf);
+
+        for(int i=0; i <= nbox; i++){
+//            std::cout << i << " " << numerov_Wf[i] << " " << analytic_Wf[i] << std::endl;
+            // Check that the box is of the same dimension for numerov and analytical
+            EXPECT_NEAR(numerov_Wf[i], analytic_Wf[i], err*100 );
+        }
+
+        ASSERT_NEAR(E_numerov, E_analytic, err*10 );
+
+    }
+
 //
     TEST(WfTest,FiniteWell){
         unsigned int nbox = 1000;
@@ -102,10 +135,41 @@ namespace {
         double E_analytic = finite_well_wf(1, nbox, width, height, analytic_Wf);
         for(int i=0; i < nbox; i++){
 //            std::cout << i << " " << numerov_Wf[i] << " " << analytic_Wf[i] << " " << finite_well_potential((-nbox/2 + i)*dx) << std::endl;
-            ASSERT_TRUE(fabs(numerov_Wf[i] - analytic_Wf[i]) < 1e-2 );
+            EXPECT_NEAR(numerov_Wf[i], analytic_Wf[i], 1e-2 );
         }
 
-        ASSERT_TRUE(fabs(E_numerov - E_analytic) < 1e-3 );
+        ASSERT_NEAR(E_numerov, E_analytic, 1e-3 );
+
+    }
+
+    TEST(WfTest,FiniteWell2){
+        unsigned int nbox = 1000;
+        double width = 7., height = 5.;
+        double *numerov_Wf = new double[nbox];
+        double *analytic_Wf = new double[nbox];
+        std::vector<double> x(nbox), pot;
+
+        for(std::vector<int>::size_type i = 0; i < x.size(); i++)
+            x[i] = dx * (int) (i - nbox / 2);
+
+//        Potential V(x, "well", 10., 5.);
+        Potential::Builder b(x);
+        Potential V = b.setType("well")
+                .setHeight(height)
+                .setWidth(width)
+                .build();
+
+        numerov_Wf[0] = 0.;
+        numerov_Wf[1] = 0.2; //later on it gets renormalized, so is just a conventional number
+        double E_numerov = solve_Numerov(0., 2., 0.01, nbox, V, numerov_Wf);
+
+        double E_analytic = finite_well_wf(1, nbox, width, height, analytic_Wf);
+        for(int i=0; i < nbox; i++){
+//            std::cout << i << " " << numerov_Wf[i] << " " << analytic_Wf[i] << " " << finite_well_potential((-nbox/2 + i)*dx) << std::endl;
+            EXPECT_NEAR(numerov_Wf[i], analytic_Wf[i], 1e-2 );
+        }
+
+        ASSERT_NEAR(E_numerov, E_analytic, 1e-3 );
 
     }
 }
