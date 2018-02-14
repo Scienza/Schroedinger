4674cc7657bfe736385c685e99d187d148508f9b
Andrea Idini <andrea.idini@gmail.com>
2018-02-14 16:55:30 +0100

    refactoring comments

--- src/Potential.cpp
+++ src/Potential.cpp
@@ -4,41 +4,6 @@ std::vector<double> Potential::get_v() {
     return this->v;
 }
 
-
-//Potential::Potential(std::vector<double> coord) : Potential(coord, "harmonic oscillator", 0.5) {}
-//Potential::Potential(std::vector<double> coord, std::string name){
-//
-//    if(name.compare("finite well potential") == 0 || name.compare("well") == 0|| name.compare("2") == 0 )
-//        Potential(coord, name, 10., 5.);
-//    else
-//        Potential(coord, name, 0.5);
-//  }
-//
-//Potential::Potential(std::vector<double> coord, std::string name, double k)
-//{
-//    std::cout << name << " " << coord.size() << std::endl;
-//
-//    this->x = coord;
-//    this->v = x;
-//    this->pot_name = name;
-//
-//    std::transform(name.begin(), name.end(), name.begin(),::tolower); //make pot_name lowercase
-//    if(name.compare("box potential") == 0 || name.compare("box") == 0 || name.compare("0") == 0)
-//        this->box_potential();
-//    else if(name.compare("harmonic oscillator") == 0 || name.compare("ho") == 0 ||  name.compare("1") == 0)
-//        this->ho_potential(k);
-//    else if(name.compare("finite well potential") == 0 || name.compare("well") == 0|| name.compare("2") == 0 ) {
-//        //        this->finite_well_potential();
-//        std::cerr << "! ERROR: wrong initialization !\n"
-//                << "! finite well potential needs two variables, not one! " << std::endl;
-//        exit(8);
-//    }
-//    else{
-//        std::cerr << "! ERROR: wrong potential name!\n! Potential" << name << "not known!" << std::endl;
-//        exit(8);
-//    }
-//}
-
 Potential::Potential(std::vector<double> coord, std::string name, double k, double width, double height){
 //    std::cout << this->pot_name << " ?" << this->x.size() << std::endl;
     this->x = coord;
