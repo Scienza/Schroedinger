c3b93ee396b7a379270d3d4d46dd5c5ff3b64923
Andrea Idini <andrea.idini@gmail.com>
2018-03-07 16:42:31 +0100

    Added Continuous.cpp and stub (no compil)

---
+++ src/Basis/ContinuousBase.cpp
@@ -0,0 +1,32 @@
+#include"ContinuousBase.h"
+
+//TODO: convert the arguments in class private properties
+ContinuousBase::ContinuousBase(double starting, double end, std::vector<double> mesh, unsigned int nbox)
+{
+    if (mesh.size() < 1 && nbox == 0)
+        throw std::invalid_argument("Empty mesh given.");
+
+//  TODO:correct in a way that you know at least one between mesh and nbox, and three defined arguments
+//    if (starting = None)
+//        throw std::invalid_argument("starting point not defined");
+//    if(end-starting < 0.)
+//        throw std::invalid_argument("Mesh boundaries not correctly specified");
+
+
+    if (mesh.size() == 1){
+        //constant mesh
+        nbox = (unsigned int) (end-starting)/mesh[0];
+
+    }else if(mesh.size() > 1){
+        throw std::invalid_argument("non--constant mesh to be developed");
+    }
+//    else if(nbox != 0){
+//
+//    }
+
+    // Due to the fact that Basis->coords is vector<double>
+    // here's the conversion from vector<int> to vector<double>
+    this->coords.reserve(nbox);
+    for(std::vector<int>::size_type i = 0; i < nbox; i++)
+        this->coords[i] = starting + mesh[0] * i;
+}
--- src/Basis/ContinuousBase.h
+++ src/Basis/ContinuousBase.h
@@ -3,34 +3,23 @@
 
 #include "Base.h"
 
-//TODO: convert the arguments in class private properties
-ContinuousBase::ContinuousBase(double starting, double end, std::vector<double> mesh, unsigned int nbox)
-{
-    if (mesh.size() < 1 && nbox == 0)
-        throw std::invalid_argument("Empty mesh given.");
+class ContinuousBase : public Base {
+public:
+    ContinuousBase::ContinuousBase(double, double, std::vector<double>, unsigned int);
 
-//  TODO:correct in a way that you know at least one between mesh and nbox, and three defined arguments
-//    if (starting = None)
-//        throw std::invalid_argument("starting point not defined");
-//    if(end-starting < 0.)
-//        throw std::invalid_argument("Mesh boundaries not correctly specified");
-
-
-    if (mesh.size() == 1){
-        //constant mesh
-        nbox = (unsigned int) (end-starting)/mesh[0];
-
-    }else if(mesh.size() > 1){
-        throw std::invalid_argument("non--constant mesh to be developed");
-    }else if(nbox != 0){
-
-    }
-
-    // Due to the fact that Basis->coords is vector<double>
-    // here's the conversion from vector<int> to vector<double>
-    this->coords.reserve(nbox);
-    for(std::vector<int>::size_type i = 0; i < nbox; i++)
-        this->coords[i] = starting + mesh[0] * i;
-}
+//    void combine() {} // @TODO implement
+//    void change(base_type t) {} // @TODO implement
+//    void set_potential() {} // @TODO implement
+//
+//    class Builder {
+//    private:
+//        std::vector< std::vector<int> > dimensions;
+//        Potential potential;
+//    public:
+//        Builder addDimension(int min_value, int max_value, int step);
+//        Builder addPotential(Potential p);
+//        DiscreteBase build();
+//    };
+};
 
 #endif //SCHROEDINGER_CONTINUOUSBASE_H
