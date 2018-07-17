e3f9ad2fe18bba2aaa5805fda70638b5f81c000b
AndreaIdini <idibrothers@gmail.com>
2018-07-17 18:43:49 +0200

    updated DiscreteBase tests

--- src/Basis/DiscreteBase.cpp
+++ src/Basis/DiscreteBase.cpp
@@ -6,13 +6,17 @@ Base::DiscreteBase::DiscreteBase(int start, int end, int step)
     if( (end-start)/step % 1 != 0)
         throw std::invalid_argument("invalid discrete basis given (check interval and step).");
 
+    if( (end-start)/step < 1)
+        throw std::invalid_argument("invalid discrete basis given (check interval and step).");
+
+
     unsigned int base_value = (end-start)/step;
     if (base_value == 0)
         throw std::invalid_argument("Empty dimensions basis given.");
 
     this-> start = start;
     this-> end   = end;
-    this-> step = step;
+    this-> step  = step;
 
     this-> quantum_numbers.reserve(base_value);
     int j=0;
