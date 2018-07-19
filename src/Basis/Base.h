65bf52c8891e8f5d9f38621c0f682d9b7a7f2d5f
Andrea Idini <andrea.idini@gmail.com>
2018-07-19 17:55:30 +0200

    Base constructor in Base.cpp file

--- src/Basis/Base.h
+++ src/Basis/Base.h
@@ -5,6 +5,8 @@
 #include <stdexcept>
 #include <iostream>
 
+class Base;
+
 class Base
 {
 public:
@@ -27,23 +29,8 @@ public:
 
 protected:
     // private Constructor, called just once
-    Base(int dimNum = 1) {
-        double start=-5., end=5., mesh=0.01;
-        baseType t = baseType::Cartesian; //TODO: Improve baseType to allow choice in input
-        this->dimNum = dimNum;
-
-        switch(t){
-          case 0  : std::cout << "Initializing Cartesian Basis" << std::endl;
-                    break;
-          case 1  : std::cout << "Initializing Spherical Basis" << std::endl;
-                    break;
-          case 2  : std::cout << "Initializing Cylindrical Basis" << std::endl;
-                    break;
-          default : throw std::invalid_argument("Wrong basis type or initialization meaningless!");
-        }
-        ContinuousBase coord(start,end,mesh);
-        this->x = coord;
-    };
+    // Base(int);
+    Base(int dimNum = 1);
 
     class DiscreteBase {
     private:
