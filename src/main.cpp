2cbea084d23a8c36c8de5bf22a62e991a1a77310
Gabriele <ga.pisciotta@gmail.com>
2018-08-05 10:27:45 +0200

    Units library included as a submodule (but not included in CMakeLists due to the fact that it doesn't compile on Windows! Make a try...), started World class.

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,10 @@
 int main(int argc, char **argv) {
 
 	BasisManager::Builder b = BasisManager::Builder();
-//	BasisManager manager = BasisManager::getInstance();
-
 	BasisManager::getInstance()->addBase( b.addDiscrete(0, 5, 1)
 					   .addContinuous(-5.0, 5.0, 0.01)
 					   .build()
 	);
-
 	std::vector<Base> basis = BasisManager::getInstance()->getBasisList();
 	return 0;
 }
